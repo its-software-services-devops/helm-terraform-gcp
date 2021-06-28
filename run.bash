4c8dc01f789ac1c47133dd47ab0426f7af66dfe3
Seubpong Monsar <pjame.fb@gmail.com>
2021-06-28 15:28:51 +0700

    Fixed rke config bug

--- run.bash
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-helm template charts \
--f test-rke-config.yaml \
---set-string rkeConfig.cloud_provider.vsphereCloudProvider.virtual_center."d2p-aaavcen01\.tdmp-server\.local".user=sysen \
---set-string rkeConfig.cloud_provider.vsphereCloudProvider.virtual_center."d2p-aaavcen01\.tdmp-server\.local".password=xxxxx \
---name-template demo --debug
